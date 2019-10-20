ae6e445bdd624d1a5a2775b5d4acb6100f9253ab
Térence Chateigné <terence.chateigne@gmail.com>
2019-10-20 17:43:05 +0200

    polyfill & https stuff removed until ...

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
-// const fs = require("fs");
-// const http = require("http");
-// const https = require("https");
+// babel/polyfill
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import cors from "cors";
 import express from "express";
 import bodyParser from "body-parser";
@@ -17,17 +17,6 @@ dotenv.config({ path: path.join(appRoot.path, ".env") });
 
 const app = express();
 
-// const privateKey = fs.readFileSync(process.env.CERT_PRIVATE_KEY, "utf8");
-// const certificate = fs.readFileSync(process.env.CERT_PUBLIC_KEY, "utf8");
-// const ca = fs.readFileSync(process.env.CERT_CHAIN, "utf8");
-// const ensureSecure = (req, res, next) => {
-//     if (req.secure) {
-//         return next();
-//     }
-//     res.redirect("https://" + req.hostname + req.url);
-// };
-
-// app.all("*", ensureSecure);
 if (process.env.NODE_ENV === "production") {
     app.use("/", express.static(path.join(appRoot.path, "client/build")));
 }
@@ -44,12 +33,6 @@ app.set("trust proxy", true);
 
 app.use(cors({ credentials: true }));
 
-// const credentials = {
-//     key: privateKey,
-//     cert: certificate,
-//     ca: ca
-// };
-
 app.use("/api", privateApi());
 app.use("/api", publicApi());
 
@@ -58,14 +41,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log("API listening on port", port);
 });
-
-// const httpServer = http.createServer(app);
-// const httpsServer = https.createServer(credentials, app);
-
-// httpServer.listen(80, () => {
-//     console.log("HTTP Server running on port 80");
-// });
-
-// httpsServer.listen(443, () => {
-//     console.log("HTTPS Server running on port 443");
-// });
